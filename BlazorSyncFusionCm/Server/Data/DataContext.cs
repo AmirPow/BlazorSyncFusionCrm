3344bb0075d11473c67b00b5000d4cad2d85e76e
AmirHosein Ahmadi <990224@AMIR-AHMADI>
2023-05-24 12:24:25 +0330

    Add project files.

---
+++ BlazorSyncFusionCm/Server/Data/DataContext.cs
@@ -0,0 +1,93 @@
+﻿using BlazorSyncFusionCm.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorSyncFusionCm.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=.\\SQLExpress;DataBase=KazimAliCrm;trusted_Connection=true;trustservercertificate=true");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Contact>().HasData(
+                new Contact
+                {
+                    Id = 11,
+                    FirstName = "Amir",
+                    LastName = "Ahmadi",
+                    NickName = "Joe",
+                    Place = "Tab",
+                    DateOfBirth = new DateTime(2022, 12, 06),
+                    DateCreated = DateTime.Now,
+                },
+                new Contact
+                {
+                    Id = 12,
+                    FirstName = "Amir2",
+                    LastName = "Ahmadi",
+                    NickName = "Joe",
+                    Place = "Tab",
+                    DateOfBirth = new DateTime(2022, 12, 06),
+                    DateCreated = DateTime.Now,
+                },
+                new Contact
+                {
+                    Id = 13,
+                    FirstName = "Amir3",
+                    LastName = "Ahmadi",
+                    NickName = "Joe",
+                    Place = "Tab",
+                    DateOfBirth = new DateTime(2022, 12, 06),
+                    DateCreated = DateTime.Now,
+                },
+                new Contact
+                {
+                    Id = 14,
+                    FirstName = "Amir4",
+                    LastName = "Ahmadi",
+                    NickName = "Joe",
+                    Place = "Tab",
+                    DateOfBirth = new DateTime(2022, 12, 06),
+                    DateCreated = DateTime.Now,
+                },
+                new Contact
+                {
+                    Id = 15,
+                    FirstName = "Amir5",
+                    LastName = "Ahmadi",
+                    NickName = "Joe",
+                    Place = "Tab",
+                    DateOfBirth = new DateTime(2022, 12, 06),
+                    DateCreated = DateTime.Now,
+                },
+                new Contact
+                {
+                    Id = 16,
+                    FirstName = "Amir6",
+                    LastName = "Ahmadi",
+                    NickName = "Joe",
+                    Place = "Tab",
+                    DateOfBirth = new DateTime(2022, 12, 06),
+                    DateCreated = DateTime.Now,
+                }
+                );
+            modelBuilder.Entity<Note>().HasData(
+
+                new Note { Id = 1, ContactId = 1, Text = "Am" },
+                new Note { Id = 2, ContactId = 2, Text = "Am" },
+                new Note { Id = 3, ContactId = 3, Text = "Am" }
+                );
+        }
+        public DbSet<Contact>contacts { get; set; }
+        public DbSet<Note> Nots { get; set; }
+
+    }
+}
---
+++ BlazorSyncFusionCm/Server/Migrations/20230524082358_Initial.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BlazorSyncFusionCm.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "contacts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NickName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Place = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateUpdated = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DateDeleted = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_contacts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Nots",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ContactId = table.Column<int>(type: "int", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Nots", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Nots_contacts_ContactId",
+                        column: x => x.ContactId,
+                        principalTable: "contacts",
+                        principalColumn: "Id");
+                });
+
+
+
+            migrationBuilder.InsertData(
+                table: "contacts",
+                columns: new[] { "Id", "DateCreated", "DateDeleted", "DateOfBirth", "DateUpdated", "FirstName", "IsDeleted", "LastName", "NickName", "Place" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2023, 5, 24, 11, 53, 58, 63, DateTimeKind.Local).AddTicks(4433), null, new DateTime(2022, 12, 6, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Amir", false, "Ahmadi", "Joe", "Tab" },
+                    { 2, new DateTime(2023, 5, 24, 11, 53, 58, 63, DateTimeKind.Local).AddTicks(4436), null, new DateTime(2022, 12, 6, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Amir2", false, "Ahmadi", "Joe", "Tab" },
+                    { 3, new DateTime(2023, 5, 24, 11, 53, 58, 63, DateTimeKind.Local).AddTicks(4438), null, new DateTime(2022, 12, 6, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Amir3", false, "Ahmadi", "Joe", "Tab" },
+                    { 4, new DateTime(2023, 5, 24, 11, 53, 58, 63, DateTimeKind.Local).AddTicks(4439), null, new DateTime(2022, 12, 6, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Amir4", false, "Ahmadi", "Joe", "Tab" },
+                    { 5, new DateTime(2023, 5, 24, 11, 53, 58, 63, DateTimeKind.Local).AddTicks(4441), null, new DateTime(2022, 12, 6, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Amir5", false, "Ahmadi", "Joe", "Tab" },
+                    { 6, new DateTime(2023, 5, 24, 11, 53, 58, 63, DateTimeKind.Local).AddTicks(4442), null, new DateTime(2022, 12, 6, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Amir6", false, "Ahmadi", "Joe", "Tab" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Nots_ContactId",
+                table: "Nots",
+                column: "ContactId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Nots");
+
+            migrationBuilder.DropTable(
+                name: "contacts");
+        }
+    }
+}
