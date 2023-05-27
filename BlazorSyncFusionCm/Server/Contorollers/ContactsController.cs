32c04b9984366bc53184859dad3dcc8c0e6d21dd
AmirHosein Ahmadi <990224@AMIR-AHMADI>
2023-05-27 12:26:44 +0330

    Complate Note

---
+++ BlazorSyncFusionCm/Server/Contorollers/ContactsController.cs
@@ -0,0 +1,81 @@
+﻿using BlazorSyncFusionCm.Server.Data;
+using BlazorSyncFusionCm.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorSyncFusionCm.Server.Contorollers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly DataContext context;
+
+        public ContactsController(DataContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Contact>>> GetAll()
+        {
+            return await context.contacts.Where(a=> !a.IsDeleted).ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Contact>> GetContactById(int id)
+        {
+            var result = await context.contacts.FindAsync(id);
+            if (result is null)
+            {
+                return NotFound("Contact Not Found");
+            }
+            return result;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Contact>> CreateContact(Contact contact)
+        {
+            try
+            {
+                context.contacts.Add(contact);
+                await context.SaveChangesAsync();
+                return Ok(contact);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            } 
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Contact>> UpdateContact(int id , Contact contact)
+        {
+            var dbContact = await context.contacts.FindAsync(id);
+            if(dbContact is null)
+            {
+                return NotFound("ContactNotDounf");
+            }
+            dbContact.FirstName = contact.FirstName;
+            dbContact.LastName = contact.LastName;
+            dbContact.NickName = contact.NickName;
+            dbContact.DateOfBirth = contact.DateOfBirth;
+            dbContact.Place = contact.Place;
+            dbContact.DateUpdated  = contact.DateUpdated;
+            await context.SaveChangesAsync();
+            return Ok(contact);
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Contact>>> DeleteContact(int id)
+        {
+            var dbContact = await context.contacts.FindAsync(id);
+            if (dbContact is null)
+            {
+                return NotFound("ContactNotDounf");
+            }
+            dbContact.IsDeleted = true;
+            dbContact.DateDeleted = DateTime.Now;
+            await context.SaveChangesAsync();
+            return await GetAll();
+        }
+    }
+}
---
+++ BlazorSyncFusionCm/Server/Contorollers/NotesController.cs
@@ -0,0 +1,60 @@
+﻿using BlazorSyncFusionCm.Server.Data;
+using BlazorSyncFusionCm.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Syncfusion.Blazor.Diagram;
+
+namespace BlazorSyncFusionCm.Server.Contorollers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotesController : ControllerBase
+    {
+        private readonly DataContext context;
+
+        public NotesController(DataContext context )
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Note>>> GetAllNotes()
+        {
+            return await context.Nots.Include(a => a.Contact)
+                .OrderByDescending(a => a.DateCreated).ToListAsync();
+        }
+
+
+        [HttpGet("{contactId}")]
+        public async Task<ActionResult<List<Note>>> GetNotesFromContact(int contactId)
+        {
+            
+            var result =  await context.Nots.Where(a => a.ContactId ==contactId)
+                .OrderByDescending(a => a.DateCreated).ToListAsync();
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Note>>> CreateNote(Note note)
+        {
+            context.Nots.Add(note);
+            await context.SaveChangesAsync();
+            return await context.Nots.Where(a => a.ContactId == note.ContactId)
+                .OrderByDescending(a => a.DateCreated).ToListAsync();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Note>>> DeleteNote(int id)
+        {
+            var dbNote= await context.Nots.FindAsync(id);
+            if(dbNote is null)
+            {
+                return NotFound("NotFound");
+            }
+            context.Nots.Remove(dbNote);
+            await context.SaveChangesAsync();
+            return await context.Nots.Where(a => a.ContactId == dbNote.ContactId)
+       .OrderByDescending(a => a.DateCreated).ToListAsync();
+        }
+    }
+}
